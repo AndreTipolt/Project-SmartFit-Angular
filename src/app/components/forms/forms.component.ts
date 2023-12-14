90e705b6c40a586d958b85ac1322e6c0fd1755f8
andretipolt <andretipoltlopes@gmail.com>
2023-12-14 16:01:33 -0300

    Add form control

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -8,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class FormsComponent implements OnInit {
 
   results = [];
-  constructor() { }
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({ // Campos do formulário
+      hour: '',
+      showClosed: false,
+    })
+  }
+
+  onSubmit(){
+    console.log(this.formGroup.value)
+  }
+
+  onClean(): void{
+    this.formGroup.reset();
   }
 
 }
